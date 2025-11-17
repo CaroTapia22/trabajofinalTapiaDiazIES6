08b5f29dad5bcd58e9ed52851929786e5f42d72b
Carolina Tapia <caro18tapia@gmail.com>
2025-11-16 22:03:51 -0300

    Agrego validaciones en Conductor.java, dependencia Validation en pom  y creo ConductorRepository

--- src/main/java/com/example/demo/model/Conductor.java
+++ src/main/java/com/example/demo/model/Conductor.java
@@ -1,92 +1,82 @@
 package com.example.demo.model;
 
 import java.time.LocalDate;
+
 import org.springframework.stereotype.Component;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
+
 
 @Component
 @Entity
 public class Conductor {
 
-    // atributos
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column
+    @NotBlank(message = "El nombre es un campo requerido")
     private String nombre;
     @Column
+    @NotBlank(message = "El apellido es un campo requerido")
+    @Size(min = 3, max = 20, message = "El apellido debe tener entre 3 y 20 caracteres")
     private String apellido;
     @Column
+    @NotBlank(message = "La licencia es un campo requerido")
     private String licencia;
     @Column
-    private Integer telefono;
+    @Pattern(regexp = "\\d+", message = "Solo se permite ingresar números")
+    private String telefono;
     @Column
     private LocalDate fechaRegistro;
+    @Column
+    private Boolean estado;   
 
 
+    //agregar @OneToOne con Vehiculo
+    //agregar @OneToMany con Viaje
 
-    // agregar relación @OneToOne con Vehiculo cuando el modelo esté creado
-    // agregar relación @OneToMany con Viaje
-
-
-    //  Constructores
     public Conductor() {
     }
 
-    public Conductor(Integer id, String nombre, String apellido, String licencia, Integer telefono, LocalDate fechaRegistro) {
+    public Conductor(Integer id, String nombre, String apellido,
+                     String licencia, String telefono, LocalDate fechaRegistro, Boolean estado) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
         this.licencia = licencia;
         this.telefono = telefono;
         this.fechaRegistro = fechaRegistro;
+        this.estado = estado;
     }
 
-    //  Getters y Setters
-    public Integer getId() {
-        return id;
-    }
-    public void setId(Integer id) {
-        this.id = id;
-    }
+    // Getter y Setters
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
 
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
 
-    public String getApellido() {
-        return apellido;
-    }
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
+    public String getApellido() { return apellido; }
+    public void setApellido(String apellido) { this.apellido = apellido; }
 
-    public String getLicencia() {
-        return licencia;
-    }
-    public void setLicencia(String licencia) {
-        this.licencia = licencia;
-    }
+    public String getLicencia() { return licencia; }
+    public void setLicencia(String licencia) { this.licencia = licencia; }
 
-    public Integer getTelefono() {
-        return telefono;
-    }
-    public void setTelefono(Integer telefono) {
-        this.telefono = telefono;
-    }
+    public String getTelefono() { return telefono; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
 
-    public LocalDate getFechaRegistro() {
-        return fechaRegistro;
-    }
-    public void setFechaRegistro(LocalDate fechaRegistro) {
-        this.fechaRegistro = fechaRegistro;
-    }
+    public LocalDate getFechaRegistro() { return fechaRegistro; }
+    public void setFechaRegistro(LocalDate fechaRegistro) { this.fechaRegistro = fechaRegistro; }
+
+    public Boolean getActivo() { return estado; }
+    public void setActivo(Boolean estado) { this.estado = estado; }
 }
---
+++ src/main/java/com/example/demo/repository/ConductorRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Conductor;
+
+@Repository
+public interface ConductorRepository extends CrudRepository<Conductor, Integer> {
+
+    // listar conductores activos
+    List<Conductor> findByActivo(Boolean activo);
+}
+
